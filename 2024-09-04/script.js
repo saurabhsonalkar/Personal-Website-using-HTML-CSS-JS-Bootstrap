59a9493d5e13bfe6afdbe10af5d1683203df1135
saurabhsonalkar <sonalkarsaurabh9@gmail.com>
2024-09-05 14:40:58 +0530

    first commit

---
+++ 2024-09-04/script.js
@@ -0,0 +1,56 @@
+/*
+$(documnet).read(function(){
+    $('#three').click(function(){
+        $('#box').slideToggle(3000)
+    })
+})
+
+*/
+
+/*
+$(document).ready(function(){
+    $('#two').animate
+    {
+    opacity:0.4
+}
+3000
+    $('#two').animate
+    {
+    fontSize:"80px"
+}3000
+    $('#two').animate
+    {
+    height:"350px"
+}3000
+})
+*/
+
+
+/*
+$(document).ready(function () {
+  $("#new").click(function () {
+    $("#two").animate;
+    {
+      fontSize: "40px";
+    }
+    duration:5000;
+  });
+  $("#old").click(function () {
+    $("#two").stop;
+  });
+
+  $("#old").click(function () {
+    $("#two").text("mine mine mine");
+  });
+});
+*/
+
+//to remove paragraph--> use .remove()
+//  to replace paragrapgh--> use .text()
+
+
+$(document).ready(function(){
+    $('#add').click(function(){
+        $('#box').removeClass('abc')
+    })
+})
